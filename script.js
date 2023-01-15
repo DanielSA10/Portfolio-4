e3702dc2d4ddd2db3cb5d1d97ee2f76479c5244b
DanielSA10 <danielstanley@live.dk>
2023-01-15 11:39:54 +0100

    Fixed animation, so it doesn't play on mobile

--- script.js
+++ script.js
@@ -64,155 +64,175 @@ tl.from('.formBrands', { 'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
 
 gsap.registerPlugin(ScrollTrigger);
 
+let mm = gsap.matchMedia();
 
+// mm.add("(min-width: 800px)", () => {
+//   gsap.to(".aboutLine", {
+//     duration: .6,
+//     width: "80%",
+//     ease: Power2.easeOut,
+//     scrollTrigger: {
+//       trigger: ".aboutMeText",
+//       start: "top 90%"
+//     }
+//   })
+// })
 
-const aboutMeTl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".aboutMeText",
-    markers: false,
-    start: "top 90%",
-    end: "bottom 520%",
-  },
-});
-
-aboutMeTl.to(".aboutLine", {
-  duration: .6,
-  width: "80%",
-  ease: Power2.easeOut 
+mm.add("(min-width: 800px)", () => {
+  const aboutMeTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".aboutMeText",
+      markers: false,
+      start: "top 90%",
+      end: "bottom 520%",
+    },
+  });
+  
+  aboutMeTl.to(".aboutLine", {
+    duration: .6,
+    width: "80%",
+    ease: Power2.easeOut 
+  })
+  aboutMeTl.from(".formAbout", {
+    'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
+  }, "<.4")
+  
+  aboutMeTl.from(".aboutMeInfo", {
+    duration: .9,
+    'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
+  })
+  aboutMeTl.from(".aboutMeImg", {
+    duration: .9,
+    'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
+  }, "<.4")
+  
+  
+  
+  const stackTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".aboutMeImg",
+      markers: false,
+      start: "top 110%",
+      end: "top 50%",
+    },
+  });
+  
+  stackTl.from(".form", {
+    'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
+    y: 0
+  })
+  
+  
+  stackTl.from(".html", {
+    duration: .4,
+    'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
+    y: 0,
+    ease: Power2.easeOut 
+  })
+  
+  
+  stackTl.from(".css", { 
+    duration: .4,
+    'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
+    y: 0,
+    ease: Power2.easeOut 
+  }, "<.2");
+  stackTl.from(".js", { 
+    duration: .4,
+    'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
+    y: 0,
+    ease: Power2.easeOut 
+  }, "<.2");
+  stackTl.from(".react", { 
+    duration: .4,
+    'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
+    y: 0,
+    ease: Power2.easeOut 
+  },"<.2");
+  stackTl.from(".figma", { 
+    duration: .4,
+    'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
+    y: 0,
+    ease: Power2.easeOut 
+  },"<.2");
+  stackTl.from(".git",{ 
+    duration: .4,
+    'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
+    y: 0,
+    ease: Power2.easeOut 
+  },"<.2");
+  stackTl.from(".wordpress",{ 
+    duration: .4,
+    'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
+    y: 0,
+    ease: Power2.easeOut 
+  },"<.2");
+  
+  
+  
+  const formprojectsTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".card",
+      markers: false,
+      start: "top bottom"
+    },
+  });
+  
+  formprojectsTl.to(".projectLine", {
+    duration: .9,
+    width: "80%",
+    ease: Power2.easeOut 
+  })
+  formprojectsTl.from(".formProjects", {
+    'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
+  }, "<.4")
+  
+  const projectsTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".form",
+      markers: false,
+      start: "top bottom"
+    },
+  });
+  
+  
+  projectsTl.from(".projectWordpress",{ 
+    duration: .9,
+    delay: 1,
+    'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
+    ease: Power2.easeOut 
+  });
+  projectsTl.from(".projectBankDashBoard",{ 
+    duration: .9,
+    'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
+    ease: Power2.easeOut 
+  }, "<.4");
+  
+  
+  const projects2Tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".form",
+      markers: false,
+      start: "top 30%",
+      end: "top 60%",
+    },
+  });
+  
+  projects2Tl.from(".projectWeatherApp",{ 
+    duration: .9,
+    'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
+    ease: Power2.easeOut 
+  });
+  projects2Tl.from(".projectPortfolio",{ 
+    duration: .9,
+    'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
+    ease: Power2.easeOut 
+  }, "<.4");
+  
 })
-aboutMeTl.from(".formAbout", {
-  'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
-}, "<.4")
-
-aboutMeTl.from(".aboutMeInfo", {
-  duration: .9,
-  'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
-})
-aboutMeTl.from(".aboutMeImg", {
-  duration: .9,
-  'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
-}, "<.4")
 
 
 
-const stackTl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".aboutMeImg",
-    markers: false,
-    start: "top 110%",
-    end: "top 50%",
-  },
-});
-
-stackTl.from(".form", {
-  'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
-  y: 0
-})
 
 
-stackTl.from(".html", {
-  duration: .4,
-  'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
-  y: 0,
-  ease: Power2.easeOut 
-})
-
-
-stackTl.from(".css", { 
-  duration: .4,
-  'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
-  y: 0,
-  ease: Power2.easeOut 
-}, "<.2");
-stackTl.from(".js", { 
-  duration: .4,
-  'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
-  y: 0,
-  ease: Power2.easeOut 
-}, "<.2");
-stackTl.from(".react", { 
-  duration: .4,
-  'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
-  y: 0,
-  ease: Power2.easeOut 
-},"<.2");
-stackTl.from(".figma", { 
-  duration: .4,
-  'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
-  y: 0,
-  ease: Power2.easeOut 
-},"<.2");
-stackTl.from(".git",{ 
-  duration: .4,
-  'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
-  y: 0,
-  ease: Power2.easeOut 
-},"<.2");
-stackTl.from(".wordpress",{ 
-  duration: .4,
-  'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
-  y: 0,
-  ease: Power2.easeOut 
-},"<.2");
-
-
-
-const formprojectsTl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".card",
-    markers: false,
-    start: "top bottom"
-  },
-});
-
-formprojectsTl.to(".projectLine", {
-  duration: .9,
-  width: "80%",
-  ease: Power2.easeOut 
-})
-formprojectsTl.from(".formProjects", {
-  'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)',
-}, "<.4")
-
-const projectsTl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".form",
-    markers: false,
-    start: "top bottom"
-  },
-});
-
-
-projectsTl.from(".projectWordpress",{ 
-  duration: .9,
-  delay: 1,
-  'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
-  ease: Power2.easeOut 
-});
-projectsTl.from(".projectBankDashBoard",{ 
-  duration: .9,
-  'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
-  ease: Power2.easeOut 
-}, "<.4");
-
-
-const projects2Tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".form",
-    markers: false,
-    start: "top 30%",
-    end: "top 60%",
-  },
-});
-
-projects2Tl.from(".projectWeatherApp",{ 
-  duration: .9,
-  'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
-  ease: Power2.easeOut 
-});
-projects2Tl.from(".projectPortfolio",{ 
-  duration: .9,
-  'clip-path': 'polygon(0% 0%, 100% 0%, 100% 0%, 0% 0%)', 
-  ease: Power2.easeOut 
-}, "<.4");
 
